845760b175cc6051a307aa21739b811cf4740eff
willemodendaal <willem@newmedialabs.co.za>
2019-09-24 10:30:49 +0200

    Simplified project-navy-cricket by removing html-loader.

--- project-navy-cricket/webpack.config.js
+++ project-navy-cricket/webpack.config.js
@@ -12,13 +12,6 @@ module.exports = {
                 }
             },
             {
-                test: /\.html/,
-                use: {
-                    loader: "html-loader",
-                    options: {minimize: true}
-                }
-            },
-            {
                 test: /\.scss/,
                 use: [
                     //Creates style nodes from js imports.
